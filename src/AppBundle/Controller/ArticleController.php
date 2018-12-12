91e90a6a6e8235f294c65e10727a7594b6f7dc36
Stephen <stephenfauvergue@gmail.com>
2018-12-12 19:35:26 +0100

    use of jms serializer

---
+++ src/AppBundle/Controller/ArticleController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Article;
+use JMS\Serializer\SerializationContext;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * @Route("/articles/{id}", name="article_show")
+     */
+    public function showAction(Article $article)
+    {
+        $data = $this->get('jms_serializer')->serialize($article, 'json',
+            SerializationContext::create()->setGroups(['detail']));
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+//    /**
+//     * @Route("/articles", name="article_create")
+//     * @Method({"POST"})
+//     */
+//    public function createAction(Request $request)
+//    {
+//        $data = $request->getContent();
+//        $article = $this->get('jms_serializer')->deserialize($data, 'AppBundle\Entity\Article', 'json');
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($article);
+//        $em->flush();
+//
+//        return new Response('', Response::HTTP_CREATED);
+//    }
+
+    /**
+     * @Route("/articles", name="article_list")
+     * @Method({"GET"})
+     */
+    public function listAction()
+    {
+        $articles = $this->getDoctrine()->getRepository('AppBundle:Article')->findAll();
+        $data = $this->get('jms_serializer')->serialize($articles, 'json',
+            SerializationContext::create()->setGroups(['list']));
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
